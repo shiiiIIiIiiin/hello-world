6fc0dc83c3d5c0df3c4a44e7adf4c3c92eb2d910
shiiiIIiIiiin <shanghaikimura@gmail.com>
2025-02-24 23:07:12 +0900

    Create test.cpp
    add test

---
+++ my_folder/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <iterator>
+#include <unordered_map>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+	string S; cin >> S;
+	for (int i = 0; i < S.size(); i++)if (S[i] == '2')cout << S[i];
+}
